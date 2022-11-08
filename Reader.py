c49c89a880a5dcbd534ac3e0f4fba4136c7449a5
Rayan Alzarqa <98127773+RayanAlzarqa@users.noreply.github.com>
2022-11-08 16:07:16 -0500

    Add files via upload

--- Reader.py
+++ Reader.py
@@ -1,16 +1,38 @@
 from pdfminer.high_level import extract_text
 import re
+import os
 
-references_to_whatever = 0
+directory = 'Pdf'
 
-text = extract_text('CPSC420_Fall2022_McElfresh.pdf').split()
+class_and_ref = {}
+# "course_title","semester","courseID","section","course_desc","department","N.Sections","year","clean_course_desc"
+keyword_list = ["sustainability","sustainable","climate change","hunger"]
 
-for word in text:
-    word = word.lower()
-    word = re.sub("[^a-zA-Z]+", "", word)
-    if word.lower().find("disability") != -1 or word.lower().find("disabilities") != -1:
-        references_to_whatever = references_to_whatever + 1
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
 
+    if os.path.isfile(f):
+        print(f)
 
-print(references_to_whatever)
+    references_to_whatever = 0
 
+    text = extract_text(f).split()
+
+    for word in text:
+        word = word.lower()
+        word = re.sub("[^a-zA-Z] + ", "", word)
+        for keyword in keyword_list:
+            if word.lower().find(keyword) != -1 :  # climate change, environment, Race
+                references_to_whatever = references_to_whatever + 1
+
+    new_key = ""
+    for i in range(2):
+        new_key = new_key + re.sub("[^a-zA-Z0-9]+", "", text[i]) + " "
+
+    class_and_ref[new_key] = references_to_whatever
+
+print("Dictionary is:", class_and_ref)
+
+for key in class_and_ref:
+    if class_and_ref[key] > 0:
+        print("Classes with a sustainability focus: ", key, "\n")
